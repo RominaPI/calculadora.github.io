6ecd9a8949c7fffda15a1bb78a8ac36f16599dfa
RominaPI <147285068+RominaPI@users.noreply.github.com>
2024-03-20 17:04:52 -0600

    Add files via upload

---
+++ indexCA.js
@@ -0,0 +1,54 @@
+
+let resultado = document.getElementById('resultado');
+
+let asignar = (valor) => {
+    resultado.value += valor;
+};
+
+let calcular = () => {
+    resultado.value = eval(resultado.value);
+};
+
+let limpiar = () => {
+    resultado.value = "";
+};
+
+let asignarPi = () => {
+    var resultado = document.getElementById('resultado');
+
+    resultado.value = Math.PI;
+};
+
+let calcularRaizCuadrada = () => {
+    var resultado = document.getElementById('resultado');
+
+    var valorResultado = parseFloat(resultado.value);
+    var raizCuadrada = Math.sqrt(valorResultado);
+
+    resultado.value = raizCuadrada;
+};
+
+let elevarAlCubo = () => {
+    var resultado = document.getElementById('resultado');
+
+    var valorResultado = parseFloat(resultado.value);
+    var resultadoCubo = Math.pow(valorResultado, 3);
+
+    resultado.value = resultadoCubo;
+};
+
+let elevarAlCuadrado = () => {
+    var resultado = document.getElementById('resultado');
+
+    var valorResultado = parseFloat(resultado.value);
+    var resultadoCubo = Math.pow(valorResultado, 2);
+
+    resultado.value = resultadoCubo;
+};
+let calcularTrigonometrica = () => {
+    var resultado = document.getElementById('resultado');
+
+    // Agregar la función de cálculo basada en la operación asignada al botón
+    resultado.value = eval(resultado.value);
+};
+
